e75b131e029d81f30ed574de69d1092dd377c232
Valentin Berlier <berlier.v@gmail.com>
2021-01-25 04:32:12 +0100

    test: fix code_load example

--- tests/examples/code_load/demo.py
+++ tests/examples/code_load/demo.py
@@ -3,4 +3,4 @@ from beet import Context, Project, ProjectBuilder, ProjectConfig
 
 def beet_default(ctx: Context):
     config = ProjectConfig(data_pack={"load": ["src"]}).resolve(ctx.directory)
-    ctx.require(ProjectBuilder(Project(config, ctx.directory)))
+    ctx.require(ProjectBuilder(Project(config, resolved_cache=ctx.cache)))
