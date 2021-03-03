504b0c4ff25f226fe13778e7399fddf2da60f273
Valentin Berlier <berlier.v@gmail.com>
2021-03-03 20:31:32 +0100

    feat: enhance pytest explanation

--- packages/lectern/lectern/pytest_plugin.py
+++ packages/lectern/lectern/pytest_plugin.py
@@ -1,3 +1,10 @@
+# type: ignore
+
+
+from _pytest.assertion.util import assertrepr_compare
+
+from lectern import Document
+
 try:
     from pytest_insta import Fmt
 except ImportError:
@@ -34,3 +41,31 @@ else:
         def dump(self, path: Path, value: Document):
             path.mkdir(exist_ok=True)
             value.save(path / "README.md", external_files=path)
+
+
+def pytest_assertrepr_compare(config, op, left, right):
+    if type(left) != type(right) or op != "==":
+        return
+
+    explanation = []
+
+    if isinstance(left, Document):
+        if left.assets != right.assets:
+            explanation += ["", "Differing resource pack:"]
+            explanation += generate_explanation(config, left.assets, right.assets)
+        if left.data != right.data:
+            explanation += ["", "Differing data pack:"]
+            explanation += generate_explanation(config, left.data, right.data)
+
+    if explanation:
+        return [assertrepr_compare(config, op, left, right)[0]] + explanation
+
+
+def generate_explanation(config, left, right):
+    summary, *explanation = config.hook.pytest_assertrepr_compare(
+        config=config, op="==", left=left, right=right
+    )[0]
+
+    yield f"  assert " + summary
+    for line in explanation:
+        yield "  " + line
