d4d3f910ce01fe8aba12d926fd887b179b9c0c5c
Valentin Berlier <berlier.v@gmail.com>
2021-03-23 03:30:11 +0100

    test: use custom snapshot format for project config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
+import json
 from pathlib import Path
 
 from pytest_insta import Fmt
 
-from beet import DataPack, ResourcePack
+from beet import DataPack, ProjectConfig, ResourcePack
+from beet.core.utils import dump_json
 
 
 class FmtResourcePack(Fmt[ResourcePack]):
@@ -23,3 +25,29 @@ class FmtDataPack(Fmt[DataPack]):
 
     def dump(self, path: Path, value: DataPack):
         value.save(path, overwrite=True)
+
+
+class FmtConfig(Fmt[ProjectConfig]):
+    extension = ".beet-config"
+
+    def load(self, path: Path) -> ProjectConfig:
+        config = ProjectConfig(**json.loads(path.read_text()))
+        self.fix_paths(config)
+        return config
+
+    def dump(self, path: Path, value: ProjectConfig):
+        path.write_text(dump_json(value.dict()))
+
+    def fix_paths(self, config: ProjectConfig):
+        if config.directory:
+            config.directory = str(Path(config.directory))
+        if config.output:
+            config.output = str(Path(config.output))
+
+        config.data_pack.load = [str(Path(p)) for p in config.data_pack.load]
+        config.resource_pack.load = [str(Path(p)) for p in config.resource_pack.load]
+        config.templates = [str(Path(p)) for p in config.templates]
+
+        for entry in config.pipeline:
+            if isinstance(entry, ProjectConfig):
+                self.fix_paths(entry)
--- tests/test_config.py
+++ tests/test_config.py
@@ -9,4 +9,4 @@ from beet.toolchain.config import load_config
 @pytest.mark.parametrize("directory", os.listdir("tests/config_examples"))  # type: ignore
 def test_config_resolution(snapshot: Any, directory: str):
     project_config = load_config(f"tests/config_examples/{directory}/beet.json")
-    assert snapshot("json") == project_config.dict()
+    assert snapshot("beet-config") == project_config
