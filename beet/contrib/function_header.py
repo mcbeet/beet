cd81eeb1807455eb827d7a23a661c314fac19e1e
Valentin Berlier <berlier.v@gmail.com>
2021-03-24 06:21:12 +0100

    refactor: expose function_header plugin factory

--- beet/contrib/function_header.py
+++ beet/contrib/function_header.py
@@ -1,16 +1,39 @@
 """Plugin that adds a header to functions automatically."""
 
 
-from beet import Context
+__all__ = [
+    "function_header",
+]
+
+
+from typing import Iterable, Optional
+
+from beet import Context, Plugin
 
 
 def beet_default(ctx: Context):
     config = ctx.meta.get("function_header", {})
-    patterns = config.get("match", [])
+
+    match = config.get("match", [])
     template = config.get("template", "function_header.mcfunction")
 
-    for path in ctx.data.functions.match(*patterns):
-        with ctx.override(render_path=path, render_group="functions"):
-            header = ctx.template.render(template)
-        function = ctx.data.functions[path]
-        function.content = header + function.text
+    ctx.require(function_header(match, template))
+
+
+def function_header(
+    match: Iterable[str] = (),
+    template: Optional[str] = "function_header.mcfunction",
+) -> Plugin:
+    """Return a plugin that adds a header to functions automatically."""
+
+    def plugin(ctx: Context):
+        if not template:
+            return
+
+        for path in ctx.data.functions.match(*match):
+            with ctx.override(render_path=path, render_group="functions"):
+                header = ctx.template.render(template)
+            function = ctx.data.functions[path]
+            function.text = header + function.text
+
+    return plugin
