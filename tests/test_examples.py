08fc9134696755fb1d305e9678909e2f72edab9d
Valentin Berlier <berlier.v@gmail.com>
2021-03-27 12:40:24 +0100

    chore: move examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,8 +6,8 @@ import pytest
 from beet import run_beet
 
 
-@pytest.mark.parametrize("directory", os.listdir("tests/examples"))  # type: ignore
+@pytest.mark.parametrize("directory", os.listdir("examples"))  # type: ignore
 def test_build(snapshot: Any, directory: str):
-    with run_beet(directory=f"tests/examples/{directory}") as ctx:
+    with run_beet(directory=f"examples/{directory}") as ctx:
         assert snapshot("data_pack") == ctx.data
         assert snapshot("resource_pack") == ctx.assets
