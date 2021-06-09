5f7b5ec9bab952edd67030dda853f709bc809a96
Valentin Berlier <berlier.v@gmail.com>
2021-06-09 09:43:04 +0200

    feat: add beet.contrib.yellow_shulker_box plugin

---
+++ beet/contrib/yellow_shulker_box.py
@@ -0,0 +1,76 @@
+"""Plugin that includes the yellow shulker box loot table."""
+
+
+from beet import Context, LootTable
+
+
+def beet_default(ctx: Context):
+    alternatives = [
+        {
+            "type": "minecraft:dynamic",
+            "name": "minecraft:contents",
+            "conditions": [
+                {
+                    "condition": "minecraft:match_tool",
+                    "predicate": {
+                        "item": "minecraft:air",
+                        "nbt": "{drop_contents:1b}",
+                    },
+                }
+            ],
+        },
+        {
+            "type": "minecraft:item",
+            "name": "minecraft:yellow_shulker_box",
+            "functions": [
+                {
+                    "function": "minecraft:copy_name",
+                    "source": "block_entity",
+                },
+                {
+                    "function": "minecraft:copy_nbt",
+                    "source": "block_entity",
+                    "ops": [
+                        {
+                            "source": "Lock",
+                            "target": "BlockEntityTag.Lock",
+                            "op": "replace",
+                        },
+                        {
+                            "source": "LootTable",
+                            "target": "BlockEntityTag.LootTable",
+                            "op": "replace",
+                        },
+                        {
+                            "source": "LootTableSeed",
+                            "target": "BlockEntityTag.LootTableSeed",
+                            "op": "replace",
+                        },
+                    ],
+                },
+                {
+                    "function": "minecraft:set_contents",
+                    "entries": [
+                        {
+                            "type": "minecraft:dynamic",
+                            "name": "minecraft:contents",
+                        }
+                    ],
+                },
+            ],
+        },
+    ]
+
+    ctx.data["minecraft:blocks/yellow_shulker_box"] = LootTable(
+        {
+            "type": "minecraft:block",
+            "pools": [
+                {
+                    "rolls": 1,
+                    "entries": [
+                        {"type": "minecraft:alternatives", "children": alternatives}
+                    ],
+                }
+            ],
+        }
+    )
