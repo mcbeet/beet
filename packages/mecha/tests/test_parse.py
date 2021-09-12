9e0277de0d49d2c543d70fa67e188d8fa4b3c6b5
Valentin Berlier <berlier.v@gmail.com>
2021-09-12 03:22:52 +0200

    test: use stable generated test names

--- packages/mecha/tests/test_parse.py
+++ packages/mecha/tests/test_parse.py
@@ -2,6 +2,7 @@ import pytest
 from beet import Function
 from beet.core.utils import JsonDict
 from pytest_insta import SnapshotFixture
+from tokenstream import InvalidSyntax
 
 from mecha import Mecha, delegate, get_argument_examples, get_command_examples
 
@@ -25,33 +26,52 @@ def test_command_examples(snapshot: SnapshotFixture, mc: Mecha):
 
 
 @pytest.mark.parametrize(
-    "argument_parser, properties, value",
+    "test_name, properties, is_error, value",
     params := [
-        (argument_parser, suite.get("properties", {}), value)
+        (
+            f"{argument_parser} {i} {j}",
+            suite.get("properties", {}),
+            suite.get("is_error", False),
+            value,
+        )
         for argument_parser, suites in get_argument_examples().items()
-        for suite in suites
-        for value in suite["examples"]
+        for i, suite in enumerate(suites)
+        for j, value in enumerate(suite["examples"])
     ],
-    ids=[f"{argument_parser}_{i}" for i, (argument_parser, _, _) in enumerate(params)],
+    ids=[args[0] for args in params],
 )
 def test_argument_examples(
     snapshot: SnapshotFixture,
     mc: Mecha,
-    argument_parser: str,
+    test_name: str,
     properties: JsonDict,
+    is_error: bool,
     value: str,
 ):
-    if f"command:argument:{argument_parser}" not in mc.spec.parsers:
+    argument_parser = f"command:argument:{test_name.partition(' ')[0]}"
+    if argument_parser not in mc.spec.parsers:
         pytest.skip()
 
     stream = mc.create_token_stream(value)
 
     with stream.syntax(literal=r"\S+"), stream.provide(properties=properties):
-        assert snapshot() == "\n---\n".join(
-            [
-                argument_parser,
-                str(properties),
-                value,
-                delegate(f"command:argument:{argument_parser}", stream).dump(),
-            ]
-        )
+        if is_error:
+            with pytest.raises(InvalidSyntax) as exc_info:
+                delegate(argument_parser, stream)
+            assert snapshot() == "\n---\n".join(
+                [
+                    test_name,
+                    str(properties),
+                    value,
+                    str(exc_info.value),
+                ]
+            )
+        else:
+            assert snapshot() == "\n---\n".join(
+                [
+                    test_name,
+                    str(properties),
+                    value,
+                    delegate(argument_parser, stream).dump(),
+                ]
+            )
