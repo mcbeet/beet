d7d7b5d5546681dc30de3ea92c118403bff3eacf
Valentin Berlier <berlier.v@gmail.com>
2021-10-09 03:11:48 +0200

    fix: don't log messages directly to avoid wrong percent formatting

--- packages/mecha/mecha/plugin.py
+++ packages/mecha/mecha/plugin.py
@@ -21,15 +21,17 @@ def beet_default(ctx: Context):
     yield
 
     for diagnostic in mc.diagnostics.exceptions:
-        message = diagnostic.format_message()
-        args = {"annotate": diagnostic.format_location()}
+        args = {
+            "message": diagnostic.format_message(),
+            "annotate": diagnostic.format_location(),
+        }
 
         if diagnostic.level == "error":
-            logger.error(message, args)
+            logger.error("%(message)s", args)
         elif diagnostic.level == "warn":
-            logger.warn(message, args)
+            logger.warn("%(message)s", args)
         elif diagnostic.level == "info":
-            logger.info(message, args)
+            logger.info("%(message)s", args)
 
     if errors := list(mc.diagnostics.get_all_errors()):
         raise DiagnosticError(DiagnosticCollection(errors), show_details=False)
