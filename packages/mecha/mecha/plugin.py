a2c27b1bf56adebb52c6af4a30553b6183e96013
Valentin Berlier <berlier.v@gmail.com>
2021-10-18 03:12:15 +0200

    fix: provide "annotate" through extra logging argument

--- packages/mecha/mecha/plugin.py
+++ packages/mecha/mecha/plugin.py
@@ -21,17 +21,15 @@ def beet_default(ctx: Context):
     yield
 
     for diagnostic in mc.diagnostics.exceptions:
-        args = {
-            "message": diagnostic.format_message(),
-            "annotate": diagnostic.format_location(),
-        }
+        args = {"message": diagnostic.format_message()}
+        extra = {"annotate": diagnostic.format_location()}
 
         if diagnostic.level == "error":
-            logger.error("%(message)s", args)
+            logger.error("%(message)s", args, extra=extra)
         elif diagnostic.level == "warn":
-            logger.warn("%(message)s", args)
+            logger.warn("%(message)s", args, extra=extra)
         elif diagnostic.level == "info":
-            logger.info("%(message)s", args)
+            logger.info("%(message)s", args, extra=extra)
 
     if errors := list(mc.diagnostics.get_all_errors()):
         raise DiagnosticError(DiagnosticCollection(errors), show_details=False)
