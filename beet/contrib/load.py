81fee160f9dfbba6d43463c260cbcffba04baca3
Valentin Berlier <berlier.v@gmail.com>
2021-11-08 03:02:35 +0100

    fix: make it possible to load packs manually

---
+++ beet/contrib/load.py
@@ -0,0 +1,32 @@
+"""Plugin that loads data packs and resource packs."""
+
+
+__all__ = [
+    "LoadOptions",
+    "load",
+]
+
+
+from typing import List
+
+from pydantic import BaseModel
+
+from beet import Context, configurable
+
+
+class LoadOptions(BaseModel):
+    resource_pack: List[str] = []
+    data_pack: List[str] = []
+
+
+def beet_default(ctx: Context):
+    ctx.require(load)
+
+
+@configurable(validator=LoadOptions)
+def load(ctx: Context, opts: LoadOptions):
+    """Plugin that loads data packs and resource packs."""
+    for config, pack in zip([opts.resource_pack, opts.data_pack], ctx.packs):
+        for pattern in config:
+            for path in ctx.directory.glob(pattern):
+                pack.load(path)
