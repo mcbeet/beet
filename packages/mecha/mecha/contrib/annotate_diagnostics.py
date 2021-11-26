1d8fca2fdd218b628f3565846b31c28d091fe1f8
Valentin Berlier <berlier.v@gmail.com>
2021-11-26 07:06:19 +0100

    feat: export annotate_diagnostics utility

--- packages/mecha/mecha/contrib/annotate_diagnostics.py
+++ packages/mecha/mecha/contrib/annotate_diagnostics.py
@@ -1,24 +1,36 @@
 """Plugin that inserts comments containing mecha diagnostics."""
 
 
+__all__ = [
+    "annotate_diagnostics",
+]
+
+
 from heapq import heappop, heappush
 from typing import Any, Dict, List, Tuple
 
 from beet import Context, TextFileBase
 
-from mecha import Mecha
+from mecha import CompilationDatabase, DiagnosticCollection, Mecha
 
 
 def beet_default(ctx: Context):
     mc = ctx.inject(Mecha)
+    annotate_diagnostics(mc.database, mc.diagnostics)
 
+
+def annotate_diagnostics(
+    database: CompilationDatabase,
+    diagnostics: DiagnosticCollection,
+):
+    """Insert formatted error messages in the original source as comments."""
     annotations: Dict[TextFileBase[Any], List[Tuple[int, int, List[str]]]] = {}
 
-    for diagnostic in mc.diagnostics.exceptions:
+    for diagnostic in diagnostics.exceptions:
         message = f"{diagnostic.level.upper():<7}{diagnostic.format_message()}\n{diagnostic.format_location()}"
 
         if diagnostic.file and not diagnostic.location.unknown:
-            if source := mc.database[diagnostic.file].source:
+            if source := database[diagnostic.file].source:
                 if code := diagnostic.format_code(source):
                     message += f"\n{code}"
 
