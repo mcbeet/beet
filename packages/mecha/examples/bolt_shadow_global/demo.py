dec104d7ef99fa783916ead6907110cf13314bab
Valentin Berlier <berlier.v@gmail.com>
2022-03-28 21:29:24 +0200

    test: bolt shadow builtin

---
+++ packages/mecha/examples/bolt_shadow_global/demo.py
@@ -0,0 +1,8 @@
+from beet import Context
+
+from mecha.contrib.bolt import Runtime
+
+
+def beet_default(ctx: Context):
+    runtime = ctx.inject(Runtime)
+    runtime.expose("max", lambda *args: args)
