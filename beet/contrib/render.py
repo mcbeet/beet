6636094655bbfd2a8dfb788df3c4e7131bca90e7
Valentin Berlier <berlier.v@gmail.com>
2022-04-13 01:21:08 +0200

    fix: no longer rely on pack attributes for render plugin

--- beet/contrib/render.py
+++ beet/contrib/render.py
@@ -12,6 +12,7 @@ from typing import Dict, List
 from pydantic import BaseModel
 
 from beet import Context, configurable
+from beet.core.utils import snake_case
 
 
 class RenderOptions(BaseModel):
@@ -26,15 +27,25 @@ def beet_default(ctx: Context):
 @configurable(validator=RenderOptions)
 def render(ctx: Context, opts: RenderOptions):
     """Plugin that processes the data pack and the resource pack with Jinja."""
-
     for groups, pack in zip([opts.resource_pack, opts.data_pack], ctx.packs):
+        file_types = {
+            file_type for namespace in pack.values() for file_type in namespace
+        }
+
+        group_map = {
+            snake_case(file_type.__name__): file_type for file_type in file_types
+        }
+        for singular in list(group_map):
+            group_map[f"{singular}s"] = group_map[singular]
+
         for group, patterns in groups.items():
             try:
-                proxy = getattr(pack, group)
+                file_type = group_map[group]
+                proxy = pack[file_type]
                 file_paths = proxy.match(*patterns)
             except:
                 raise ValueError(f"Invalid render group {group!r}.") from None
             else:
                 for path in file_paths:
                     with ctx.override(render_path=path, render_group=group):
-                        ctx.template.render_file(proxy[path])
+                        ctx.template.render_file(proxy[path])  # type: ignore
