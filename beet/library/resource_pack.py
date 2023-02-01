e399d4788f88ca1937fab4b495b4510238623374
Valentin Berlier <berlier.v@gmail.com>
2023-02-01 05:06:03 +0100

    Revert "test: add tests + enable atlases"
    This reverts commit 0d5648b21b2b7fe668ffa2bfb026d874147f2dd7.

--- beet/library/resource_pack.py
+++ beet/library/resource_pack.py
@@ -334,7 +334,6 @@ class ResourcePackNamespace(Namespace):
     textures:         NamespacePin[Texture]        = NamespacePin(Texture)
     sounds:           NamespacePin[Sound]          = NamespacePin(Sound)
     particles:        NamespacePin[Particle]       = NamespacePin(Particle)
-    atlases:          NamespacePin[Atlas]          = NamespacePin(Atlas)
     # fmt: on
 
     @classmethod
@@ -384,5 +383,4 @@ class ResourcePack(Pack[ResourcePackNamespace]):
     textures:         NamespaceProxyDescriptor[Texture]        = NamespaceProxyDescriptor(Texture)
     sounds:           NamespaceProxyDescriptor[Sound]          = NamespaceProxyDescriptor(Sound)
     particles:        NamespaceProxyDescriptor[Particle]       = NamespaceProxyDescriptor(Particle)
-    atlases:          NamespaceProxyDescriptor[Atlas]          = NamespaceProxyDescriptor(Atlas)
     # fmt: on
