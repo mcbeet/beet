0d1e9f37b85b3dd6222fa00ba707438f6a85074f
Valentin Berlier <berlier.v@gmail.com>
2023-05-27 05:04:14 +0200

    feat: add nested text resources

--- packages/mecha/examples/basic_nested_resources/src/data/demo/functions/foo.mcfunction
+++ packages/mecha/examples/basic_nested_resources/src/data/demo/functions/foo.mcfunction
@@ -39,3 +39,52 @@ function_tag demo:abc {
         "demo:foo"
     ]
 }
+
+glsl_shader minecraft:include/matrix:
+    #version 150
+
+    mat2 mat2_rotate_z(float radians) {
+        return mat2(
+            cos(radians), -sin(radians),
+            sin(radians), cos(radians)
+        );
+    }
+
+fragment_shader minecraft:core/blit_screen:
+    #version 150
+
+    uniform sampler2D DiffuseSampler;
+
+    uniform vec4 ColorModulator;
+
+    in vec2 texCoord;
+    in vec4 vertexColor;
+
+    out vec4 fragColor;
+
+    void main() {
+        vec4 color = texture(DiffuseSampler, texCoord) * vertexColor;
+
+        // blit final output of compositor into displayed back buffer
+        fragColor = color * ColorModulator;
+    }
+
+vertex_shader minecraft:core/blit_screen:
+    #version 150
+
+    in vec3 Position;
+    in vec2 UV;
+    in vec4 Color;
+
+    uniform mat4 ModelViewMat;
+    uniform mat4 ProjMat;
+
+    out vec2 texCoord;
+    out vec4 vertexColor;
+
+    void main() {
+        gl_Position = ProjMat * ModelViewMat * vec4(Position, 1.0);
+
+        texCoord = UV;
+        vertexColor = Color;
+    }
--- packages/mecha/mecha/contrib/nested_resources.py
+++ packages/mecha/mecha/contrib/nested_resources.py
@@ -2,21 +2,35 @@
 
 
 __all__ = [
+    "NestedResources",
     "NestedResourcesTransformer",
+    "AstNestedText",
+    "parse_nested_text",
 ]
 
 
 from dataclasses import dataclass, replace
-from typing import Dict, List, Type
+from textwrap import dedent
+from typing import Dict, List, Type, Union
 
-from beet import Context, DataModelBase, Generator, NamespaceFile, TagFile
-from beet.core.utils import required_field, snake_case
-from tokenstream import set_location
+from beet import (
+    Context,
+    DataModelBase,
+    DataPack,
+    Generator,
+    NamespaceFile,
+    ResourcePack,
+    TagFile,
+    TextFileBase,
+)
+from beet.core.utils import JsonDict, required_field, snake_case
+from tokenstream import InvalidSyntax, Token, TokenStream, set_location
 
 from mecha import (
     AstChildren,
     AstCommand,
     AstJson,
+    AstNode,
     AstResourceLocation,
     AstRoot,
     CompilationDatabase,
@@ -24,81 +38,166 @@ from mecha import (
     Mecha,
     MultilineParser,
     MutatingReducer,
+    consume_line_continuation,
     delegate,
     rule,
 )
 
-NESTED_JSON_COMMAND_TREE = {
-    "type": "literal",
-    "children": {
-        "name": {
-            "type": "argument",
-            "parser": "minecraft:resource_location",
-            "children": {
-                "content": {
-                    "type": "argument",
-                    "parser": "mecha:nested_json",
-                    "executable": True,
-                }
-            },
-        }
-    },
-}
-
 
 def beet_default(ctx: Context):
     ctx.require("mecha.contrib.nesting")
+    ctx.inject(NestedResources).prepare(ctx.generate)
 
-    mc = ctx.inject(Mecha)
 
-    json_resources = {
-        snake_case(file_type.__name__): file_type
-        for pack in ctx.packs
-        for file_type in pack.resolve_scope_map().values()
-        if issubclass(file_type, DataModelBase)
-    }
+class NestedResources:
+    mc: Mecha
+    json_resources: Dict[str, Type[NamespaceFile]]
+    text_resources: Dict[str, Type[NamespaceFile]]
 
-    # Make sure there's no confusion between nested resources and existing commands.
-    for name, tree in mc.spec.tree.get_all_literals():
-        if name in json_resources and any(tree.get_all_arguments()):
-            json_resources[f"{name}_file"] = json_resources.pop(name)
+    def __init__(
+        self,
+        arg: Union[Mecha, Context],
+        *packs: Union[ResourcePack, DataPack],
+    ):
+        if isinstance(arg, Context):
+            packs += arg.packs
+            self.mc = arg.inject(Mecha)
+        else:
+            self.mc = arg
 
-    commands = {name: NESTED_JSON_COMMAND_TREE for name in json_resources}
-    tag_commands = {
-        name: NESTED_JSON_COMMAND_TREE
-        for name, file_type in json_resources.items()
-        if issubclass(file_type, TagFile)
-    }
+        nested_resources = {
+            snake_case(file_type.__name__): file_type
+            for pack in packs
+            for file_type in pack.resolve_scope_map().values()
+        }
 
-    mc.spec.add_commands(
-        {
-            "type": "root",
+        # Make sure there's no confusion between nested resources and existing commands.
+        for name, tree in self.mc.spec.tree.get_all_literals():
+            if name in nested_resources and any(tree.get_all_arguments()):
+                nested_resources[f"{name}_file"] = nested_resources.pop(name)
+
+        self.json_resources = {
+            name: file_type
+            for name, file_type in nested_resources.items()
+            if issubclass(file_type, DataModelBase)
+        }
+
+        for name in self.json_resources:
+            del nested_resources[name]
+
+        self.text_resources = {
+            name: file_type
+            for name, file_type in nested_resources.items()
+            if issubclass(file_type, TextFileBase)
+        }
+
+    def prepare(self, generate: Generator):
+        json_commands = {
+            name: self.create_command_tree_fragment("mecha:nested_json")
+            for name in self.json_resources
+        }
+
+        tag_commands = {
+            name: self.create_command_tree_fragment("mecha:nested_json")
+            for name, file_type in self.json_resources.items()
+            if issubclass(file_type, TagFile)
+        }
+
+        text_commands = {
+            name: self.create_command_tree_fragment("mecha:nested_text")
+            for name in self.text_resources
+        }
+
+        self.mc.spec.add_commands(
+            {
+                "type": "root",
+                "children": {
+                    **json_commands,
+                    "merge": {"type": "literal", "children": json_commands},
+                    "append": {"type": "literal", "children": tag_commands},
+                    "prepend": {"type": "literal", "children": tag_commands},
+                    **text_commands,
+                },
+            }
+        )
+
+        self.mc.spec.parsers["nested_json"] = delegate("json")
+        self.mc.spec.parsers["command:argument:mecha:nested_json"] = MultilineParser(
+            delegate("nested_json")
+        )
+
+        self.mc.spec.parsers["nested_text"] = parse_nested_text
+        self.mc.spec.parsers["command:argument:mecha:nested_text"] = MultilineParser(
+            delegate("nested_text")
+        )
+
+        self.mc.transform.extend(
+            NestedResourcesTransformer(
+                generate=generate,
+                database=self.mc.database,
+                nested_resource_identifiers={
+                    f"{prefix}{name}:name:content": file_type
+                    for name, file_type in self.json_resources.items()
+                    | self.text_resources.items()
+                    for prefix in [""]
+                    + ["merge:"] * issubclass(file_type, DataModelBase)
+                    + ["append:", "prepend:"] * issubclass(file_type, TagFile)
+                },
+            )
+        )
+
+    @staticmethod
+    def create_command_tree_fragment(parser: str) -> JsonDict:
+        return {
+            "type": "literal",
             "children": {
-                **commands,
-                "merge": {"type": "literal", "children": commands},
-                "append": {"type": "literal", "children": tag_commands},
-                "prepend": {"type": "literal", "children": tag_commands},
+                "name": {
+                    "type": "argument",
+                    "parser": "minecraft:resource_location",
+                    "children": {
+                        "content": {
+                            "type": "argument",
+                            "parser": parser,
+                            "executable": True,
+                        }
+                    },
+                }
             },
         }
-    )
 
-    mc.spec.parsers["nested_json"] = delegate("json")
-    mc.spec.parsers["command:argument:mecha:nested_json"] = MultilineParser(
-        delegate("nested_json")
-    )
 
-    mc.transform.extend(
-        NestedResourcesTransformer(
-            generate=ctx.generate,
-            database=mc.database,
-            json_identifiers={
-                f"{prefix}{name}:name:content": file_type
-                for name, file_type in json_resources.items()
-                for prefix in ["", "merge:"]
-                + ["append:", "prepend:"] * issubclass(file_type, TagFile)
-            },
-        )
+@dataclass(frozen=True, slots=True)
+class AstNestedText(AstNode):
+    """Ast nested text node."""
+
+    value: str = required_field()
+
+
+def parse_nested_text(stream: TokenStream) -> AstNestedText:
+    with stream.syntax(colon=r":"):
+        colon = stream.expect("colon")
+
+    with stream.intercept("newline"), stream.syntax(text=r"[^\s].*", comment=None):
+        if not consume_line_continuation(stream):
+            exc = InvalidSyntax("Expected non-empty block.")
+            raise set_location(exc, colon)
+
+        start_pos = stream.current.location.pos
+        level = stream.indentation[-2]
+
+        lines: List[Token] = []
+
+        while True:
+            lines.append(stream.expect("text"))
+
+            with stream.provide(multiline=True, line_indentation=level):
+                if not consume_line_continuation(stream):
+                    break
+
+    node = AstNestedText(
+        value=dedent(stream.source[start_pos : lines[-1].end_location.pos] + "\n")
     )
+    return set_location(node, lines[0], lines[-1])
 
 
 @dataclass
@@ -108,7 +207,7 @@ class NestedResourcesTransformer(MutatingReducer):
     generate: Generator = required_field()
     database: CompilationDatabase = required_field()
 
-    json_identifiers: Dict[str, Type[NamespaceFile]] = required_field()
+    nested_resource_identifiers: Dict[str, Type[NamespaceFile]] = required_field()
 
     @rule(AstRoot)
     def nested_resources(self, node: AstRoot):
@@ -116,15 +215,17 @@ class NestedResourcesTransformer(MutatingReducer):
         commands: List[AstCommand] = []
 
         for command in node.commands:
-            if file_type := self.json_identifiers.get(command.identifier):
+            if file_type := self.nested_resource_identifiers.get(command.identifier):
                 name, content = command.arguments
 
                 if isinstance(name, AstResourceLocation) and isinstance(
-                    content, AstJson
+                    content, (AstJson, AstNestedText)
                 ):
                     full_name = name.get_canonical_value()
                     file_instance = file_type(
-                        content.evaluate(),
+                        content.evaluate()
+                        if isinstance(content, AstJson)
+                        else content.value,
                         original=self.database.current.original,
                     )
 
@@ -163,7 +264,7 @@ class NestedResourcesTransformer(MutatingReducer):
     def execute_nested_resource(self, node: AstCommand):
         if (
             isinstance(subcommand := node.arguments[0], AstCommand)
-            and subcommand.identifier in self.json_identifiers
+            and subcommand.identifier in self.nested_resource_identifiers
         ):
             d = Diagnostic("error", "Nested resource not allowed behind execute.")
             raise set_location(d, subcommand, subcommand.arguments[0])
