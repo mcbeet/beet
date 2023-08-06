8275c7e6929b64ae0c49d061a49f5fc7eedb1c5d
Valentin Berlier <berlier.v@gmail.com>
2023-08-06 13:16:47 +0200

    feat: add beet.contrib.log_level

---
+++ beet/contrib/log_level.py
@@ -0,0 +1,22 @@
+"""Plugin for setting the log level."""
+
+
+import logging
+from typing import Literal
+
+from beet import Context, PluginOptions, configurable
+
+
+class LogLevelOptions(PluginOptions):
+    level: Literal["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
+
+
+def beet_default(ctx: Context):
+    ctx.require(log_level)
+
+
+@configurable(validator=LogLevelOptions)
+def log_level(ctx: Context, opts: LogLevelOptions):
+    """Plugin for setting the log level."""
+    logger = logging.getLogger()
+    logger.setLevel(opts.level)
