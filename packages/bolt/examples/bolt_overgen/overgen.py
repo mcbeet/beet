71fa68216d23446ab9c0b3b9d4c8db4a122bf01c
Valentin Berlier <berlier.v@gmail.com>
2023-10-31 00:50:15 +0100

    test: overlay generator modules

---
+++ packages/bolt/examples/bolt_overgen/overgen.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from typing import Any, ClassVar, Iterable, List, Optional, Tuple, Union
+
+from beet import Context, DataPack, ResourcePack, TextFile, TextFileBase
+from beet.core.utils import FormatsRangeDict, normalize_string
+from mecha import CompilationDatabase, CompilationUnit, Mecha
+
+from bolt import Runtime
+
+
+class Overgen(TextFile):
+    scope: ClassVar[tuple[str, ...]] = ("overgen",)
+    extension: ClassVar[str] = ".bolt"
+
+
+def define_custom_resources(ctx: Context):
+    ctx.data.extend_namespace.append(Overgen)
+
+
+def provide_compilation_units(
+    pack: Union[ResourcePack, DataPack],
+    match: Optional[List[str]] = None,
+) -> Iterable[Tuple[TextFileBase[Any], CompilationUnit]]:
+    for resource_location in pack[Overgen].match(*match or ["*"]):
+        file_instance = pack[Overgen][resource_location]
+
+        yield file_instance, CompilationUnit(
+            resource_location=resource_location,
+            pack=pack.overlays[normalize_string(resource_location)],
+        )
+
+
+def define_compilation_unit_providers(ctx: Context):
+    mc = ctx.inject(Mecha)
+    mc.providers = [provide_compilation_units]
+
+
+@dataclass
+class DeclareOverlayFormats:
+    database: CompilationDatabase
+
+    def __call__(self, min_inclusive: int, max_inclusive: int):
+        pack = self.database[self.database.current].pack
+        if pack is not None:
+            pack.supported_formats = FormatsRangeDict(
+                min_inclusive=min_inclusive,
+                max_inclusive=max_inclusive,
+            )
+
+
+def define_module_globals(ctx: Context):
+    mc = ctx.inject(Mecha)
+    declare_overlay_formats = DeclareOverlayFormats(mc.database)
+
+    runtime = ctx.inject(Runtime)
+    runtime.expose("declare_overlay_formats", declare_overlay_formats)
+
+
+def remove_custom_resources(ctx: Context):
+    ctx.data[Overgen].clear()
---
+++ packages/bolt/tests/snapshots/examples__build_bolt_overgen__0.pack.md
@@ -0,0 +1,49 @@
+# Lectern snapshot
+
+## Data pack
+
+`@data_pack pack.mcmeta`
+
+```json
+{
+  "pack": {
+    "pack_format": 18,
+    "description": ""
+  },
+  "overlays": {
+    "entries": [
+      {
+        "formats": {
+          "min_inclusive": 18,
+          "max_inclusive": 19
+        },
+        "directory": "demo_stuff"
+      }
+    ]
+  }
+}
+```
+
+### demo
+
+`@function demo:foo`
+
+```mcfunction
+value = "abc"
+print(value)
+say this should be left untouched
+```
+
+## Overlay `demo_stuff`
+
+`@overlay demo_stuff`
+
+### demo
+
+`@function demo:foo`
+
+```mcfunction
+say stuff
+```
+
+`@endoverlay`
