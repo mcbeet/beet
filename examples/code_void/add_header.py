6db43ad2822359236ea42506593f8a6a8686d350
edayot <pro.e.dayot@gmail.com>
2024-04-04 16:53:53 +0200

    style: black

--- examples/code_void/add_header.py
+++ examples/code_void/add_header.py
@@ -1,13 +1,9 @@
-
-
 from beet import Context, Function
-
 
 
 def beet_default(ctx: Context):
     for f in ctx.data.functions.keys():
-        ctx.data.functions[f]=Function(f"say {f}")
-    
+        ctx.data.functions[f] = Function(f"say {f}")
+
     ctx.data.functions["namespace:"] = Function("give me sugar")
     ctx.data.functions["namespace:a/"] = Function("give me apple")
-
