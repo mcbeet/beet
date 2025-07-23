fdc40caee625f18e6ae88101694682426c6f177f
edayot <pro.e.dayot@gmail.com>
2025-07-23 20:28:03 +0200

    black

--- tests/test_pack_registry_container_type.py
+++ tests/test_pack_registry_container_type.py
@@ -1,6 +1,5 @@
-
-
 from beet.resources.pack_format_registry import data_pack_format_registry
+
 
 def test_registry_data():
     assert data_pack_format_registry.normalize_key("1.21") == (1, 21)
@@ -9,4 +8,3 @@ def test_registry_data():
 
     assert data_pack_format_registry.missing((1, 20, "x")) == 41
     assert data_pack_format_registry.missing((1, 19, "x")) == 12
-
