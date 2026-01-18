33fdb8e59c31674ed159a79abeb68c8e7e48e0f3
Yavanni <thenuclearnexus@gmail.com>
2026-01-17 19:34:49 -0500

    fix: use match for bake_macros

--- packages/mecha/src/mecha/contrib/bake_macros.py
+++ packages/mecha/src/mecha/contrib/bake_macros.py
@@ -139,35 +139,36 @@ class MacroBaker(Visitor):
         result: List[AstCommand|AstError] = []
         modified = False
 
-        for command in node.commands:
-            if isinstance(command, AstError):
-                result.append(command)
-                continue
-            
-            if isinstance(command, AstMacroLine) and invocation:
-                baked_macro_line = self.bake_macro_line(command, invocation)
-                modified |= baked_macro_line is not command
-                command = baked_macro_line
+        for c in node.commands:
+            match c:
+                case AstError() as error:
+                    result.append(error)
+                    continue
+                case AstCommand() as command:
+                    if isinstance(command, AstMacroLine) and invocation:
+                        baked_macro_line = self.bake_macro_line(command, invocation)
+                        modified |= baked_macro_line is not command
+                        command = baked_macro_line
 
-            args = command.arguments
-            stack: List[AstCommand] = [command]
+                    args = command.arguments
+                    stack: List[AstCommand] = [command]
 
-            while args and isinstance(subcommand := args[-1], AstCommand):
-                stack.append(subcommand)
-                args = subcommand.arguments
+                    while args and isinstance(subcommand := args[-1], AstCommand):
+                        stack.append(subcommand)
+                        args = subcommand.arguments
 
-            last = stack[-1]
+                    last = stack[-1]
 
-            if last.identifier == "function:name:arguments":
-                last = self.bake_macro_invocation(last)
-                if last is not stack.pop():
-                    for prefix in reversed(stack):
-                        args = AstChildren([*prefix.arguments[:-1], last])
-                        last = replace(prefix, arguments=args)
-                    modified = True
-                    command = last
+                    if last.identifier == "function:name:arguments":
+                        last = self.bake_macro_invocation(last)
+                        if last is not stack.pop():
+                            for prefix in reversed(stack):
+                                args = AstChildren([*prefix.arguments[:-1], last])
+                                last = replace(prefix, arguments=args)
+                            modified = True
+                            command = last
 
-            result.append(command)
+                    result.append(command)
 
         if invocation:
             invocation_compilation_unit = self.database[invocation.file_instance]
